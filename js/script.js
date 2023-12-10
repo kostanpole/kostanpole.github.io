f7723fe2014323ae17273c81b6f6717a42e532e4
kostanpole <77924505+kostanpole@users.noreply.github.com>
2023-12-10 15:29:24 +0300

    Delete js directory

--- js/script.js
+++
@@ -1,7 +0,0 @@
-
-document.querySelector('.singkapvo-gesionalem').onmousemove = function (e) {
-var x = e.pageX - e.target.offsetLeft;
-var y = e.pageY - e.target.offsetTop;
-e.target.style.setProperty('--x', x + 'px');
-e.target.style.setProperty('--y', y + 'px');
-};
